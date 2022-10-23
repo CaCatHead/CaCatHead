7170ee7d849c46fb109c97dd5d912a9a29db17e1
XLor <yjl9903@vip.qq.com>
2022-10-23 21:49:59 +0800

    feat(server): testcases

--- server/CaCatHead/user/tests.py
+++ server/CaCatHead/user/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class UserAuthTests(TestCase):
+    client = APIClient()
+
+    def test_hello_world(self):
+        resp = self.client.get('/api/hello/')
+        assert resp.data['message'] == 'Hello, world!'
