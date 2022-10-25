5b4f01b2aa09b04db7d5fbfbc03e2f3309965210
XLor <yjl9903@vip.qq.com>
2022-10-25 20:37:24 +0800

    ci(server): add init db

--- server/CaCatHead/user/tests.py
+++ server/CaCatHead/user/tests.py
@@ -8,3 +8,12 @@ class UserAuthTests(TestCase):
     def test_hello_world(self):
         resp = self.client.get('/api/hello/')
         assert resp.data['message'] == 'Hello, world!'
+
+    def test_login(self):
+        resp = self.client.post('/api/auth/login', {"username": "root",
+                                                    "password": "12345678"})
+
+        assert len(resp.data['expiry']) > 0
+        assert len(resp.data['token']) > 0
+
+        # TODO: send GET request to /api/user/profile to check TOKEN is valid
