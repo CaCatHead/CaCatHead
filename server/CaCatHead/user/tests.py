485976e9e168231a2de9cf620b8953dc6a1142f0
czswez <1154953652@qq.com>
2022-10-26 19:12:36 +0800

    chore(server): test setUpTestData

--- server/CaCatHead/user/tests.py
+++ server/CaCatHead/user/tests.py
@@ -1,27 +1,25 @@
-import random
-
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 
 
-def init_superuser(username='root', password='12345678', email='root@example.com'):
-    User.objects.create_superuser(username=username, email=email, password=password)
-
-
 class UserAuthTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_superuser(username='root', password='12345678', email='root@example.com')
+
     def test_hello_world(self):
         resp = self.client.get('/api/hello/')
         assert resp.status_code == 200
         assert resp.data['message'] == 'Hello, world!'
 
     def test_login(self):
-        init_superuser()
         resp = self.client.post('/api/auth/login',
                                 {"username": "root", "password": "12345678"},
                                 format='json')
         assert resp.status_code == 200
         assert len(resp.data['expiry']) > 0
         assert len(resp.data['token']) > 0
+        print(resp.data)
         # 测试Token访问/api/user/profile 返回200
         authorization = "Token " + resp.data['token']
         self.client.credentials(HTTP_AUTHORIZATION=authorization)
@@ -30,7 +28,6 @@ class UserAuthTests(APITestCase):
         self.assertEqual(resp2.data, {'status': 'ok', 'user': {'username': 'root', 'email': 'root@example.com'}})
 
     def test_token_error_authentication_fail(self):
-        init_superuser()
         resp = self.client.post('/api/auth/login',
                                 {"username": "root", "password": "12345678"},
                                 format='json')
@@ -45,7 +42,6 @@ class UserAuthTests(APITestCase):
         self.assertEqual(resp2.status_code, 401)
 
     def test_password_error(self):
-        init_superuser()
         resp = self.client.post('/api/auth/login',
                                 {"username": "root", "password": "gdxtxdy"},
                                 format='json')
@@ -53,7 +49,6 @@ class UserAuthTests(APITestCase):
         self.assertEqual(resp.data['detail'], "不正确的身份认证信息。")
 
     def test_username_error(self):
-        init_superuser()
         resp = self.client.post('/api/auth/login',
                                 {"username": "gdx", "password": "gdxtxdy"},
                                 format='json')
@@ -61,7 +56,6 @@ class UserAuthTests(APITestCase):
         assert resp.data['detail'] == "不正确的身份认证信息。"
 
     def test_sql_inject_fail(self):
-        init_superuser()
         resp = self.client.post('/api/auth/login',
                                 {"username": "root; DELETE FROM User", "password": "12345678"},
                                 format='json')
@@ -85,7 +79,6 @@ class UserAuthTests(APITestCase):
         """
         登录获取 token，退出，该 token 不可用
         """
-        init_superuser()
         resp = self.client.post('/api/auth/login', {"username": "root", "password": "12345678"},
                                 format='json')
         assert resp.status_code == 200
@@ -103,7 +96,6 @@ class UserAuthTests(APITestCase):
         """
         多次登录 登录了就带token 第二次登录返回已登录
         """
-        init_superuser()
         resp = self.client.post('/api/auth/login',
                                 {"username": "root", "password": "12345678"},
                                 format='json')
@@ -127,7 +119,6 @@ class UserAuthTests(APITestCase):
         """
         多次登录 不带token 每个独立
         """
-        init_superuser()
         authorizations = []
         # 多次登录
         for _ in range(0, 10):
