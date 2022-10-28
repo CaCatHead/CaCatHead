36ec227127b1016ca2b0485683330328c2df0724
XLor <yjl9903@vip.qq.com>
2022-10-28 19:03:29 +0800

    feat(server): public post api

--- server/CaCatHead/post/views.py
+++ server/CaCatHead/post/views.py
@@ -9,12 +9,27 @@ from CaCatHead.post.serializers import PostSerializer, PostContentSerializer
 
 @api_view()
 def list_post(request: Request):
+    """
+    列出用户可见的所有公告
+    """
     posts = Post.objects.filter_user(user=request.user)
     return Response({'status': 'ok', 'posts': PostSerializer(posts, many=True).get_or_raise()})
 
 
 @api_view()
-def get_post(request: Request, post_id):
+def list_public_post():
+    """
+    列出公开的公告
+    """
+    posts = Post.objects.filter_public()
+    return Response({'status': 'ok', 'posts': PostSerializer(posts, many=True).get_or_raise()})
+
+
+@api_view()
+def get_post_content(request: Request, post_id):
+    """
+    查看公告内容
+    """
     post = Post.objects.filter_user(user=request.user, id=post_id).first()
     return Response({'status': 'ok', 'post': PostContentSerializer(post).get_or_raise()})
 
@@ -22,4 +37,7 @@ def get_post(request: Request, post_id):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated, IsAdminUser])
 def create_post(request: Request):
+    """
+    创建公告
+    """
     return Response({'status': 'ok'})
--- server/CaCatHead/urls.py
+++ server/CaCatHead/urls.py
@@ -35,7 +35,8 @@ urlpatterns = [
     path('api/user/profile', user_views.current_user_profile),
     # post
     path('api/posts', post_views.list_post),
-    path('api/post/<int:post_id>', post_views.get_post),
+    path('api/posts/public', post_views.list_public_post),
+    path('api/post/<int:post_id>', post_views.get_post_content),
     path('api/post', post_views.create_post)
 ]
 
