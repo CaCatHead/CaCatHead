e4c596f9765e41f43875fa07d6c34fd1f475b4ea
XLor <yjl9903@vip.qq.com>
2022-10-28 17:23:48 +0800

    feat(server): permission views

--- server/CaCatHead/permission/manager.py
+++ server/CaCatHead/permission/manager.py
@@ -14,23 +14,30 @@ class PermissionManager(models.Manager):
         query_set = self.get_queryset()
         return query_set.filter(is_public=True, **kwargs)
 
-    def _q_user_private(self, **kwargs):
-        assert 'user' in kwargs
-        user = kwargs['user']
+    def _q_user_private(self, user: User, **kwargs):
         permission_subquery = UserPermission.objects.filter(user=user, content_type=self.model_name())
-        del kwargs['user']
         return Q(is_public=False, id__in=Subquery(permission_subquery.values('content_id')), **kwargs)
 
-    def _q_group_private(self, **kwargs):
-        assert 'group' in kwargs
-        group = kwargs['group']
+    def _q_group_private(self, group: Group, **kwargs):
         permission_subquery = GroupPermission.objects.filter(group=group, content_type=self.model_name())
-        del kwargs['group']
         return Q(is_public=False, id__in=Subquery(permission_subquery.values('content_id')), **kwargs)
 
     def filter_user(self, **kwargs):
-        query_user = self._q_user_private(**kwargs)
-        return self.get_queryset().filter(Q(is_public=True) | query_user)
+        assert 'user' in kwargs
+        user = kwargs['user']
+        kwargs.pop('user', None)
+        kwargs.pop('group', None)
+        kwargs.pop('groups', None)
+        if user is None or not user.is_authenticated:
+            # 未认证用户只能看到公开内容
+            return self.filter_public(**kwargs)
+        elif user.is_active and (user.is_superuser or user.is_staff):
+            # 超级用户或管理用户, 直接返回
+            return self.get_queryset().filter(**kwargs)
+        else:
+            # 其他用户, 查询用户拥有的权限
+            query_user = self._q_user_private(user, **kwargs)
+            return self.get_queryset().filter(Q(**kwargs), Q(is_public=True) | query_user)
 
     def grant_user_permission(self, user: User, permission: str, content_id):
         user_permission = UserPermission(user=user, content_type=self.model_name(), content_id=content_id,
--- server/CaCatHead/post/views.py
+++ server/CaCatHead/post/views.py
@@ -9,13 +9,13 @@ from CaCatHead.post.serializers import PostSerializer, PostContentSerializer
 
 @api_view()
 def list_post(request: Request):
-    posts = Post.objects.filter_public()
+    posts = Post.objects.filter_user(user=request.user)
     return Response({'status': 'ok', 'posts': PostSerializer(posts, many=True).data})
 
 
 @api_view()
 def get_post(request: Request, post_id):
-    posts = Post.objects.filter(is_public=True, id=post_id).first()
+    posts = Post.objects.filter_user(user=request.user, id=post_id).first()
     return Response({'status': 'ok', 'post': PostContentSerializer(posts).data})
 
 
