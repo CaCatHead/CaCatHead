8e2e7f65040876f63da4f53a2cd4875604f854eb
XLor <yjl9903@vip.qq.com>
2022-11-08 02:03:33 +0800

    feat(deploy): wait for mysql start

--- deploy/server/docker-entrypoint.sh
+++ deploy/server/docker-entrypoint.sh
@@ -9,7 +9,7 @@ export DB_PASS=$(cat $DB_PASS_FILE)
 
 if [ "$1" = "server" ] ; then
   # wait mysql bootstrap
-  sleep 10
+  ./wait
   /usr/src/.venv/bin/python ./manage.py migrate
   /usr/src/.venv/bin/python ./manage.py collectstatic --no-input
   /usr/src/.venv/bin/uvicorn CaCatHead.asgi:application --workers 4 --host 0.0.0.0 --port 8000
