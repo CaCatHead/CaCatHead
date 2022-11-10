215d8c30ebbee1384b581e17ef3d73f9c681f1d4
XLor <yjl9903@vip.qq.com>
2022-11-10 20:40:12 +0800

    chore(app): add zip lib

--- app/pages/polygon/problem/[id]/testcase.vue
+++ app/pages/polygon/problem/[id]/testcase.vue
@@ -4,6 +4,26 @@ import type { FullPolygonProblem } from '@/composables/types';
 const props = defineProps<{ problem: FullPolygonProblem }>();
 
 const { problem } = toRefs(props);
+
+const files = ref<File[]>([]);
+
+const onUploadTestcase = (ev: Event) => {
+  const target = ev.target as HTMLInputElement;
+  const uploaded = target.files;
+};
 </script>
 
-<template></template>
+<template>
+  <div>
+    <div>
+      <c-file-input id="testcase" @change="onUploadTestcase" multiple
+        >上传测试用例</c-file-input
+      >
+    </div>
+    <div>
+      <div v-for="file in files">
+        <span>{{ file.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
