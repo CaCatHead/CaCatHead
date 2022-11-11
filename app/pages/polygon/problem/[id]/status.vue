a8c219a121c07daa3ab22241744e2e9d5fae550b
XLor <yjl9903@vip.qq.com>
2022-11-11 19:39:59 +0800

    chore(app): update status table style

--- app/pages/polygon/problem/[id]/status.vue
+++ app/pages/polygon/problem/[id]/status.vue
@@ -16,14 +16,27 @@ const submissions = ref(data.value?.submissions ?? []);
   <div>
     <c-table :data="submissions">
       <template #headers>
-        <c-table-header name="id" label="#"></c-table-header>
-        <c-table-header name="verdict" label="结果"></c-table-header>
+        <c-table-header
+          name="id"
+          label="#"
+          width="64px"
+          row-class="text-center"
+        ></c-table-header>
+        <c-table-header
+          name="verdict"
+          label="结果"
+          row-class="text-center"
+        ></c-table-header>
       </template>
 
       <template #id="{ row }">
-        <nuxt-link :to="`/polygon/submission/${row.id}`">{{
-          row.id
-        }}</nuxt-link>
+        <nuxt-link
+          :to="`/polygon/submission/${row.id}`"
+          text-sky-700
+          text-op-70
+          hover:text-op-100
+          >{{ row.id }}</nuxt-link
+        >
       </template>
       <template #verdict="{ row }">{{ row.verdict }}</template>
     </c-table>
