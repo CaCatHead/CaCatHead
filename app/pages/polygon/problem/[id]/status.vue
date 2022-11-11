bc05762d35569fecb4265cdc8ea895bd50850b00
XLor <yjl9903@vip.qq.com>
2022-11-11 19:04:18 +0800

    feat(app): init polygon submissions

--- app/pages/polygon/problem/[id]/status.vue
+++ app/pages/polygon/problem/[id]/status.vue
@@ -4,8 +4,26 @@ import type { FullPolygonProblem } from '@/composables/types';
 const props = defineProps<{ problem: FullPolygonProblem }>();
 
 const { problem } = toRefs(props);
+
+const { data } = useFetchAPI<{ sumbissions: any[] }>(
+  `/api/polygon/${problem.value.id}/submissions`
+);
+
+const submissions = ref(data.value?.sumbissions ?? []);
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <c-table :data="submissions">
+      <template #headers>
+        <c-table-header name="id" label="#"></c-table-header>
+      </template>
+
+      <template #id="{ row }">
+        <nuxt-link :to="`/polygon/submission/${row.id}`">{{
+          row.id
+        }}</nuxt-link>
+      </template>
+    </c-table>
+  </div>
 </template>
