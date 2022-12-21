ad5fc9fa97020215431980c9e0b7086aedea6c7f
XLor <yjl9903@vip.qq.com>
2022-12-21 23:23:32 +0800

    feat: init docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,66 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'CaCatHead',
+  description: '一个开源的在线评测系统',
+  head: [
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'icon', href: '/favicon.png' }],
+  ],
+  lastUpdated: true,
+  themeConfig: {
+    logo: '/favicon.svg',
+    editLink: {
+      pattern: 'https://github.com/XLoJ/CaCatHead/tree/main/docs/:path',
+      text: '反馈修改建议',
+    },
+    footer: {
+      message: 'Released under the AGPL-3.0 License.',
+      copyright: 'Copyright © 2021-PRESENT XLor',
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/XLoJ/CaCatHead' },
+    ],
+    // algolia: {
+    //   appId: 'FGCMJD7ZM9',
+    //   apiKey: 'dad73f46ec1ba55810109fb2fa7a472b',
+    //   indexName: 'docs',
+    // },
+    nav: [
+      { text: '部署', link: '/deploy/' },
+      { text: '使用文档', link: '/usage/' },
+      { text: '体验', link: 'https://oj.xlorpaste.cn' },
+    ],
+    sidebar: {
+      '/': [
+        {
+          text: '介绍',
+          items: [{ text: '猫猫头', link: '/intro/' }],
+        },
+        {
+          text: '部署',
+          items: [
+            {
+              text: '开始',
+              link: '/deploy/',
+            },
+          ],
+        },
+        {
+          text: '使用',
+          items: [
+            {
+              text: '文档',
+              link: '/usage/',
+            },
+          ],
+        },
+        {
+          text: '系统设计',
+          items: [],
+        },
+      ],
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,33 @@
+---
+layout: home
+sidebar: false
+
+title: 主页
+titleTemplate: 猫猫头
+
+hero:
+  name: CaCatHead
+  text: 猫猫头
+  tagline: 一个开源的在线评测系统
+  image:
+    src: /favicon.png
+    alt: CaCatHead
+  actions:
+    - theme: brand
+      text: 开始
+      link: /intro/
+    - theme: alt
+      text: 试一试
+      link: https://oj.xlorpaste.cn
+    - theme: alt
+      text: GitHub
+      link: https://github.com/XLoJ/CaCatHead
+
+# features:
+#   - title: 自动化
+#     details: 自动抓取 / 下载 / 上传动画资源
+#   - title: 无服务器架构
+#     details: 你无需购买服务器来部署
+#   - title: 集成媒体库
+#     details: 本地资源可被 Jellyfin, Plex 等软件自动识别
+---
