0b4bc5fcf653817cddcf73841de4cd7e74e86734
XLor <yjl9903@vip.qq.com>
2023-01-09 17:57:18 +0800

    feat(app): init help page

---
+++ app/components/QA.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div shadow-box rounded px4 py4 space-y-2>
+    <div>
+      <span font-bold text-xl mr4>Q</span>
+      <slot name="q"></slot>
+    </div>
+    <div>
+      <span font-bold text-xl mr4>A</span>
+      <slot name="a"></slot>
+    </div>
+  </div>
+</template>
--- app/pages/help.vue
+++ app/pages/help.vue
@@ -4,4 +4,21 @@ useHead({
 });
 </script>
 
-<template>帮助</template>
+<template>
+  <div !max-w-full !w-full prose prose-truegray dark:prose-invert>
+    <h2 text-center>常见问题及解答</h2>
+    <div space-y-4>
+      <q-a>
+        <template #q>这是什么网站？</template>
+        <template #a
+          >这是<a href="https://github.com/XLoJ/CaCatHead">猫猫头</a
+          >，是一个开源的在线评测系统。</template
+        >
+      </q-a>
+      <q-a>
+        <template #q>CaCatHead Online Judge 支持哪些语言？</template>
+        <template #a>目前为止，你可以使用 C, C++, Java。</template>
+      </q-a>
+    </div>
+  </div>
+</template>
