5488fc92d77063b760b1ef5eb849df0c8f763a64
XLor <yjl9903@vip.qq.com>
2023-01-09 23:45:26 +0800

    fix(app): submission detail mobile

--- app/components/Submission/Detail.vue
+++ app/components/Submission/Detail.vue
@@ -28,31 +28,51 @@ const { submission } = toRefs(props);
         v-for="(testcase, index) in submission.detail.results"
         px4
         py4
-        flex
+        sm:flex
         justify-between
         font-mono
       >
-        <h4 w="1/5" font-600>
-          {{ testcase.sample ? '样例' : '测试点' }} #{{ index + 1 }}
-        </h4>
-        <verdict :verdict="testcase.verdict" w="1/5"></verdict>
-        <span
-          w="1/5"
-          inline-flex
+        <div
+          w="2/5"
+          lt-sm="w-full justify-between mb1"
+          flex
           items-center
-          v-if="testcase.score !== undefined && testcase.score !== null"
+          justify-center
         >
-          <span i-carbon-checkmark-outline text-lg mr1></span>
-          <span>{{ testcase.score }} pts</span>
-        </span>
-        <span w="1/5" inline-flex items-center>
-          <span i-carbon-time text-lg mr1></span>
-          <span>{{ testcase.time }} ms</span>
-        </span>
-        <span w="1/5" inline-flex items-center>
-          <span i-carbon-chip text-lg mr1></span>
-          <span>{{ testcase.memory }} KB</span>
-        </span>
+          <h4 w="1/2" font-600>
+            {{ testcase.sample ? '样例' : '测试点' }} #{{ index + 1 }}
+          </h4>
+          <verdict
+            :verdict="testcase.verdict"
+            w="1/2"
+            lt-sm="text-right"
+          ></verdict>
+        </div>
+        <div
+          w="3/5"
+          lt-sm="w-full justify-between"
+          flex
+          items-center
+          justify-center
+        >
+          <span
+            w="1/3"
+            inline-flex
+            items-center
+            v-if="testcase.score !== undefined && testcase.score !== null"
+          >
+            <span i-carbon-checkmark-outline text-lg mr1></span>
+            <span>{{ testcase.score }} pts</span>
+          </span>
+          <span w="1/3" inline-flex items-center lt-sm="justify-center">
+            <span i-carbon-time text-lg mr1></span>
+            <span>{{ testcase.time }} ms</span>
+          </span>
+          <span w="1/3" inline-flex items-center lt-sm="justify-end">
+            <span i-carbon-chip text-lg mr1></span>
+            <span>{{ testcase.memory }} KB</span>
+          </span>
+        </div>
       </div>
     </div>
     <div v-if="submission.detail.results" text-sm text-right>
