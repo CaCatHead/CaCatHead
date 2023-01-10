30e7505712e663284eeeb99ef6049e971ca02cf3
XLor <yjl9903@vip.qq.com>
2023-01-10 17:42:26 +0800

    fix(app): notification overlap

--- app/components/C/Notification/Provider.vue
+++ app/components/C/Notification/Provider.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { INotification } from '@/composables/notify';
 
+import { useAutoAnimate } from '@formkit/auto-animate/vue';
+
 const props = withDefaults(
   defineProps<{
     align?: 'justify-center' | 'justify-start' | 'justify-end';
@@ -13,18 +15,21 @@ const notifications = ref<INotification[]>([]);
 provide(NotificationProviderSymbol, {
   notifications,
 });
+
+const [parent] = useAutoAnimate();
 </script>
 
 <template>
   <div>
     <slot></slot>
-    <div fixed top-0 left-0 w-screen space-y-4 p4>
+    <div fixed top-0 left-0 w-screen ref="parent" style="pointer-events: none">
       <div
         v-for="notification in notifications"
         :key="notification.timestamp.toISOString()"
-        h="0"
+        mt2
         flex
-        :class="[props.align]"
+        justify-center
+        style="pointer-events: none"
       >
         <c-notification :color="notification.color" z="1000">{{
           notification.message
