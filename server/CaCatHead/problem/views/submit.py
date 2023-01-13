0cf86424ae7bed73340392e360a56107886f42aa
XLor <yjl9903@vip.qq.com>
2023-01-13 16:30:07 +0800

    feat(server): split repo and polygon submit

--- server/CaCatHead/problem/views/submit.py
+++ server/CaCatHead/problem/views/submit.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 from rest_framework.exceptions import APIException
 
 from CaCatHead.core.constants import Verdict
-from CaCatHead.judge.tasks import judge_polygon_submission
+from CaCatHead.judge.tasks import judge_polygon_submission, judge_repository_submission
 from CaCatHead.problem.models import ProblemRepository, Problem
 from CaCatHead.submission.models import Submission
 
 logger = logging.getLogger(__name__)
 
 
-def submit_problem_code(user: User, repo: ProblemRepository, problem: Problem, payload: dict):
+def submit_problem_code(is_repo: bool, user: User, repo: ProblemRepository, problem: Problem, payload: dict):
     code = payload['code']
     language = payload['language']
     submission = Submission(
@@ -24,15 +24,31 @@ def submit_problem_code(user: User, repo: ProblemRepository, problem: Problem, p
     )
     submission.save()
 
-    try:
-        judge_polygon_submission.delay(submission.id)
-        return submission
-    except judge_polygon_submission.OperationalError as ex:
-        logger.exception('Sending task raised: %r', ex)
-        raise APIException(detail='提交代码失败', code=400)
+    if is_repo:
+        try:
+            judge_repository_submission.delay(submission.id)
+            return submission
+        except judge_repository_submission.OperationalError as ex:
+            logger.exception('Sending task raised: %r', ex)
+            raise APIException(detail='提交题库代码失败', code=400)
+    else:
+        try:
+            judge_polygon_submission.delay(submission.id)
+            return submission
+        except judge_polygon_submission.OperationalError as ex:
+            logger.exception('Sending task raised: %r', ex)
+            raise APIException(detail='提交 Polygon 代码失败', code=400)
 
 
-def rejudge_problem_code(submission: Submission):
+def submit_repository_problem_code(user: User, repo: ProblemRepository, problem: Problem, payload: dict):
+    return submit_problem_code(True, user, repo, problem, payload)
+
+
+def submit_polygon_problem_code(user: User, repo: ProblemRepository, problem: Problem, payload: dict):
+    return submit_problem_code(False, user, repo, problem, payload)
+
+
+def rejudge_problem_code(is_repo: bool, submission: Submission):
     submission.judged = None
     submission.verdict = Verdict.Waiting
     submission.score = 0
@@ -41,9 +57,25 @@ def rejudge_problem_code(submission: Submission):
     submission.detail = {}
     submission.save()
 
-    try:
-        judge_polygon_submission.delay(submission.id)
-        return submission
-    except judge_polygon_submission.OperationalError as ex:
-        logger.exception('Sending task raised: %r', ex)
-        raise APIException(detail='重测代码失败', code=400)
+    if is_repo:
+        try:
+            judge_repository_submission.delay(submission.id)
+            return submission
+        except judge_repository_submission.OperationalError as ex:
+            logger.exception('Sending task raised: %r', ex)
+            raise APIException(detail='重测题库代码失败', code=400)
+    else:
+        try:
+            judge_polygon_submission.delay(submission.id)
+            return submission
+        except judge_polygon_submission.OperationalError as ex:
+            logger.exception('Sending task raised: %r', ex)
+            raise APIException(detail='重测 Polygon 代码失败', code=400)
+
+
+def rejudge_repository_problem_code(submission: Submission):
+    return rejudge_problem_code(True, submission)
+
+
+def rejudge_polygon_problem_code(submission: Submission):
+    return rejudge_problem_code(False, submission)
