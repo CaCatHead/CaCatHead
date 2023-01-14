9ad6f8906e110022aa60aa5993ce36ab372424f7
yjl9903 <yjl9903@outlook.com>
2023-01-14 17:43:19 +0800

    chore: update manage script

--- manage.sh
+++ manage.sh
@@ -2,10 +2,24 @@
 
 case "$1" in
   "up")
-    docker compose up --build -d
+    if [ -z "$2" ] ; then
+      docker compose up --build -d
+    else
+      docker compose up "$2" --build -d
+    fi
     ;;
   "judge")
-    docker compose -f docker-compose.judge.yml --profile catjudge up --build -d
+    if [ -z "$2" ] ; then
+      docker compose -f docker-compose.judge.yml --profile catjudge up --build -d
+    elif [ "$2" = "stop" ] ; then
+      docker stop "catjudge_node"
+    elif [ "$2" = "restart" ] ; then
+      docker restart "catjudge_node"
+    elif [ "$2" = "logs" ] ; then
+      docker logs "catjudge_node" -f
+    elif [ "$2" = "exec" ] ; then
+      docker exec -it "catjudge_node" /bin/bash 
+    fi
     ;;
   "ps")
     docker compose ps
@@ -27,8 +41,6 @@ case "$1" in
   "logs")
     if [ -z "$2" ] ; then
       docker compose logs -f
-    elif [ "$2" = "node" ] ; then 
-      docker logs "catjudge_node" -f
     else
       docker logs "cacathead_$2" -f
     fi
