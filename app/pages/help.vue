1ca992f5f5720b9d1ef1b02a31344c457466c25d
XLor <yjl9903@vip.qq.com>
2023-01-16 14:00:53 +0800

    chore(app): fix help java code

--- app/pages/help.vue
+++ app/pages/help.vue
@@ -80,11 +80,9 @@ const JavaExample = `import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner cin = new Scanner(System.in);
-    while (cin.hasNextInt()) {
-      int a = cin.nextInt();
-      int b = cin.nextInt();
-      System.out.println(a + b);
-    }
+    int a = cin.nextInt();
+    int b = cin.nextInt();
+    System.out.println(a + b);
   }
 }`;
 </script>
