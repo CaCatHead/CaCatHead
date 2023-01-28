210c668cd51cb6b4ed545d553f7b413d1f6c8fcf
XLor <yjl9903@vip.qq.com>
2023-01-28 23:18:28 +0800

    chore(app): add some icons

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -32,7 +32,10 @@ const posts = ref(data.value?.posts ?? []);
       </div>
 
       <div shadow-box rounded>
-        <h3 border="b-1 base" p4 text-xl font-bold>最近比赛</h3>
+        <h3 border="b-1 base" p4 text-xl font-bold flex items-center gap1>
+          <span i-carbon-calendar></span>
+          <span>最近比赛</span>
+        </h3>
         <div text-sm>
           <c-table :data="data?.recent_contests" :mobile="false">
             <template #headers>
@@ -53,7 +56,10 @@ const posts = ref(data.value?.posts ?? []);
       </div>
 
       <div shadow-box rounded>
-        <h3 border="b-1 base" p4 text-xl font-bold>牛逼网友</h3>
+        <h3 border="b-1 base" p4 text-xl font-bold flex items-center gap1>
+          <span i-carbon-user-avatar></span>
+          <span>牛逼网友</span>
+        </h3>
         <div text-sm>
           <c-table :data="data?.top_users" :mobile="false">
             <template #headers>
@@ -71,7 +77,10 @@ const posts = ref(data.value?.posts ?? []);
       </div>
 
       <div shadow-box rounded>
-        <h3 border="b-1 base" p4 text-xl font-bold>最新动态</h3>
+        <h3 border="b-1 base" p4 text-xl font-bold flex items-center gap1>
+          <span i-carbon-recently-viewed></span>
+          <span>最新动态</span>
+        </h3>
         <div text-sm>
           <c-table :data="data?.recent_posts" :mobile="false">
             <template #headers>
