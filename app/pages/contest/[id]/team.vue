4778a5a9cddc60ecc90b99fcc7bf633a3824a6ea
XLor <yjl9903@vip.qq.com>
2023-03-10 17:19:39 +0800

    feat(app): init team manage

---
+++ app/pages/contest/[id]/team.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { FullContest, ContestExtraInfo } from '@/composables/types';
+
+const route = useRoute();
+
+const notify = useNotification();
+
+const props = defineProps<{
+  contest: FullContest;
+  extra_info: ContestExtraInfo | null;
+}>();
+
+const { contest } = toRefs(props);
+
+const file = ref([] as File[]);
+
+const uploadTeams = async () => {};
+</script>
+
+<template>
+  <div>
+    <div>
+      <c-file-input
+        id="teams"
+        v-model="file"
+        accept=".csv"
+        @change="uploadTeams"
+        >导入队伍列表</c-file-input
+      >
+    </div>
+  </div>
+</template>
