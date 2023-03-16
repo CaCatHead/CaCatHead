68f6bef60738cc65aed0c8c3d09f7f99ae12d9e5
XLor <yjl9903@vip.qq.com>
2023-03-16 19:08:26 +0800

    feat(server): contest admin

--- server/CaCatHead/contest/admin.py
+++ server/CaCatHead/contest/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from CaCatHead.contest.models import Contest
+from CaCatHead.contest.models import Contest, ContestRegistration, Team
 
 
 class ContestAdmin(admin.ModelAdmin):
@@ -11,4 +11,18 @@ class ContestAdmin(admin.ModelAdmin):
     search_fields = ('id', 'title')
 
 
+class ContestRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'team', 'contest', 'score', 'dirty')
+
+    list_display_links = ('id', 'name',)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'rating')
+
+    list_display_links = ('id', 'name')
+
+
 admin.site.register(Contest, ContestAdmin)
+admin.site.register(ContestRegistration, ContestRegistrationAdmin)
+admin.site.register(Team, TeamAdmin)
