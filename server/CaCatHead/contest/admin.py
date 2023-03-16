2eecdd3778bfdbb806c34b8cded5f83a0aa46f58
XLor <yjl9903@vip.qq.com>
2023-03-16 19:09:05 +0800

    chore(server): format

--- server/CaCatHead/contest/admin.py
+++ server/CaCatHead/contest/admin.py
@@ -4,23 +4,23 @@ from CaCatHead.contest.models import Contest, ContestRegistration, Team
 
 
 class ContestAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'start_time', 'end_time', 'owner', 'is_public')
+    list_display = ('id', 'title', 'start_time', 'end_time', 'owner', 'is_public',)
 
     list_display_links = ('id', 'title',)
 
-    search_fields = ('id', 'title')
+    search_fields = ('id', 'title',)
 
 
 class ContestRegistrationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'team', 'contest', 'score', 'dirty')
+    list_display = ('id', 'name', 'team', 'contest', 'score', 'dirty',)
 
     list_display_links = ('id', 'name',)
 
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'owner', 'rating')
+    list_display = ('id', 'name', 'owner', 'rating',)
 
-    list_display_links = ('id', 'name')
+    list_display_links = ('id', 'name',)
 
 
 admin.site.register(Contest, ContestAdmin)
